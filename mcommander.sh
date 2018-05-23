17581793fefaf69cebf481cb35d921d04664b4df
Graham Inggs <ginggs@debian.org>
2018-05-24 00:06:19 +0200

    Updated for NAS4Free 11.1.0.4

--- mcommander.sh
+++ mcommander.sh
@@ -8,6 +8,7 @@
 # date:         2013-05-05 ; Switch from mc-light to mc ; drop compat7x ; add libslang
 # date:         2013-08-10 ; Update mc package name to mc-4.8.8.tbz
 # date:         2013-08-23 ; Fetch files from packages-9.2-release ; add libssh2
+# date:         2018-05-23 ; Updated for NAS4Free 11.1.0.4
 # purpose:      Install Midnight Commander on NAS4Free (embedded version).
 # Note:         Check the end of the page.
 #
@@ -15,10 +16,12 @@
 DIR=`dirname $0`;
 PLATFORM=`uname -m`
 RELEASE=`uname -r | cut -d- -f1`
-URL="ftp://ftp.freebsd.org/pub/FreeBSD/ports/${PLATFORM}/packages-9.2-release/All"
-MCFILE="mc-4.8.8.tbz"
-LIBSLANGFILE="libslang2-2.2.4_4.tbz"
-LIBSSH2FILE="libssh2-1.4.3_1,2.tbz"
+REL_MAJOR=`echo $RELEASE | cut -d. -f1`
+REL_MINOR=`echo $RELEASE | cut -d. -f2`
+URL="http://distcache.freebsd.org/FreeBSD:${REL_MAJOR}:${PLATFORM}/release_${REL_MINOR}/All"
+MCFILE="mc-4.8.19_2.txz"
+LIBSLANGFILE="libslang2-2.3.1.txz"
+LIBSSH2FILE="libssh2-1.8.0,3.txz"
 #----------------------- Set Errors ---------------------------------------------------------------------
 _msg() { case $@ in
   0) echo "The script will exit now."; exit 0 ;;
@@ -32,43 +35,46 @@ if [ ! `echo $0 |cut -c1-5` = "/mnt/" ]; then _msg 4 ; fi
 cd $DIR;
 #----------------------- Download and decompress mc files if don't exist --------------------------------
 FILE=${MCFILE}
-if [ ! -d ${DIR}/bin ]; then
+if [ ! -d ${DIR}/usr/local/bin ]; then
   if [ ! -e ${DIR}/${FILE} ]; then fetch ${URL}/${FILE} || _msg 1; fi
-  if [ -f ${DIR}/${FILE} ]; then tar xzf ${DIR}/${FILE} || _msg 2; rm ${DIR}/+*; rm -R ${DIR}/man; fi
-  if [ ! -d ${DIR}/bin ]; then _msg 4; fi
+  if [ -f ${DIR}/${FILE} ]; then tar xzf ${DIR}/${FILE} || _msg 2; rm ${DIR}/+*;
+    rm -R ${DIR}/usr/local/man; fi
+  if [ ! -d ${DIR}/usr/local/bin ]; then _msg 4; fi
 fi
 #----------------------- Download and decompress libslang files if don't exist --------------------------
 FILE=${LIBSLANGFILE}
-if [ ! -d ${DIR}/lib ]; then
+if [ ! -d ${DIR}/usr/local/lib ]; then
   if [ ! -e ${DIR}/${FILE} ]; then fetch ${URL}/${FILE} || _msg 1; fi
   if [ -f ${DIR}/${FILE} ]; then tar xzf ${DIR}/${FILE} || _msg 2};
-    rm ${DIR}/+*; rm -R ${DIR}/libdata; rm -R ${DIR}/man; rm -R ${DIR}/include; rm ${DIR}/lib/*.a;
-    rm ${DIR}/bin/slsh; rm ${DIR}/etc/slsh.rc; fi
-  if [ ! -d ${DIR}/lib ]; then _msg 4; fi
+    rm ${DIR}/+*; rm -R ${DIR}/usr/local/libdata; rm -R ${DIR}/usr/local/man;
+    rm -R ${DIR}/usr/local/include; rm ${DIR}/usr/local/lib/*.a; rm ${DIR}/usr/local/bin/slsh;
+    rm ${DIR}/usr/local/etc/slsh.rc; fi
+  if [ ! -d ${DIR}/usr/local/lib ]; then _msg 4; fi
 fi
 #----------------------- Download and decompress libssh2 files if don't exist ---------------------------
 FILE=${LIBSSH2FILE}
-if [ ! -f ${DIR}/lib/libssh2.so ]; then
+if [ ! -f ${DIR}/usr/local/lib/libssh2.so ]; then
   if [ ! -e ${DIR}/${FILE} ]; then fetch ${URL}/${FILE} || _msg 1; fi
   if [ -f ${DIR}/${FILE} ]; then tar xzf ${DIR}/${FILE} || _msg 2};
-    rm ${DIR}/+*; rm -R ${DIR}/libdata; rm -R ${DIR}/man; rm -R ${DIR}/include; rm ${DIR}/lib/*.a;
-    rm ${DIR}/lib/*.la; fi
-  if [ ! -d ${DIR}/lib ]; then _msg 4; fi
+    rm ${DIR}/+*; rm -R ${DIR}/usr/local/libdata; rm -R ${DIR}/usr/local/man;
+    rm -R ${DIR}/usr/local/include; rm ${DIR}/usr/local/lib/*.a; fi
+  if [ ! -d ${DIR}/usr/local/lib ]; then _msg 4; fi
 fi
 #----------------------- Create symlinks ----------------------------------------------------------------
-if [ ! -e /usr/local/share/mc ]; then ln -s ${DIR}/share/mc /usr/local/share; fi
-if [ ! -e /usr/local/libexec/mc ]; then ln -s ${DIR}/libexec/mc /usr/local/libexec; fi
-if [ ! -e /usr/local/etc/mc ]; then ln -s ${DIR}/etc/mc /usr/local/etc; fi
-for i in `ls $DIR/bin/`
-  do if [ ! -e /usr/local/bin/${i} ]; then ln -s ${DIR}/bin/$i /usr/local/bin; fi; done
-for i in `ls $DIR/share/locale`
+if [ ! -e /usr/local/share/mc ]; then ln -s ${DIR}/usr/local/share/mc /usr/local/share; fi
+if [ ! -e /usr/local/libexec/mc ]; then ln -s ${DIR}/usr/local/libexec/mc /usr/local/libexec; fi
+if [ ! -e /usr/local/etc/mc ]; then ln -s ${DIR}/usr/local/etc/mc /usr/local/etc; fi
+for i in `ls $DIR/usr/local/bin/`
+  do if [ ! -e /usr/local/bin/${i} ]; then ln -s ${DIR}/usr/local/bin/$i /usr/local/bin; fi; done
+for i in `ls $DIR/usr/local/share/locale`
   do if [ ! -e /usr/local/share/locale/${i} ]; then
-      ln -s ${DIR}/share/locale/${i} /usr/local/share/locale;
+      ln -s ${DIR}/usr/local/share/locale/${i} /usr/local/share/locale;
     else if [ ! -e /usr/local/share/locale/${i}/LC_MESSAGES/mc.mo ]; then
-      ln -s ${DIR}/share/locale/${i}/LC_MESSAGES/mc.mo /usr/local/share/locale/${i}/LC_MESSAGES; fi;
+      ln -s ${DIR}/usr/local/share/locale/${i}/LC_MESSAGES/mc.mo \
+        /usr/local/share/locale/${i}/LC_MESSAGES; fi;
     fi; done
-for i in `ls $DIR/lib`
-  do if [ ! -e /usr/local/lib/${i} ]; then ln -s $DIR/lib/$i /usr/local/lib; fi; done
+for i in `ls $DIR/usr/local/lib`
+  do if [ ! -e /usr/local/lib/${i} ]; then ln -s ${DIR}/usr/local/lib/$i /usr/local/lib; fi; done
 _msg 3 ; exit 0;
 #----------------------- End of Script ------------------------------------------------------------------
 # 1. Keep this script in his own directory.
